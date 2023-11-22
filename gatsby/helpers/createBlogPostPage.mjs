6255d4dde9bfc947072ba4d917b146ea79bc3481
Bartosz Łaniewski <git@laniewski.me>
2023-11-22 23:43:17 +0100

    feat: generate thumbnails (#941)

--- gatsby/helpers/createBlogPostPage.mjs
+++ gatsby/helpers/createBlogPostPage.mjs
@@ -1,10 +1,18 @@
 import path from "path";
 
+import { createImage } from "./createImage.mjs";
+import { mapSlugToImageName } from "./mapSlugToImageName.mjs";
+
 export async function createBlogPostPage(actions, data) {
   const slug = data.fields.slug;
-  const template = path.resolve(`./src/templates/blog-post.tsx`);
+  const template = path.resolve("./src/templates/blog-post.tsx");
 
   console.info(`Creating blog post page at ${slug}`);
+
+  await createImage({
+    title: data.frontmatter.title,
+    output: `./static/thumbnails/${mapSlugToImageName(slug)}.png`,
+  });
 
   return actions.createPage({
     path: slug,
