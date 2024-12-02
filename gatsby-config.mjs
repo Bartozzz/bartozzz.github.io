2c9355d8c695667be89003f53aa8b9d2c7edc442
Bartosz Łaniewski <git@laniewski.me>
2024-12-02 23:22:34 +0100

    feat: implement clickable permalinks

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -53,7 +53,38 @@ const config = {
             [rehypeKatex, { strict: "ignore" }],
             rehypePrism,
             rehypeSlug,
-            rehypeAutolinkHeadings,
+            [
+              rehypeAutolinkHeadings,
+              {
+                behavior: "append",
+                properties: (node) => ({
+                  className: ["heading-link"], // Add custom class if needed
+                  ariaLabel: `Permalink to "${node.children.map((child) => child.value || "").join("")}"`, // Extract heading text dynamically
+                }),
+                content: () => ({
+                  type: "element",
+                  tagName: "svg",
+                  // https://primer.style/foundations/icons/link-16
+                  properties: {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    viewBox: "0 0 16 16",
+                    width: "16",
+                    height: "16",
+                    fill: "currentColor",
+                    "aria-hidden": "true",
+                  },
+                  children: [
+                    {
+                      type: "element",
+                      tagName: "path",
+                      properties: {
+                        d: "m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z",
+                      },
+                    },
+                  ],
+                }),
+              },
+            ],
             [
               rehypeExternalLinks,
               {
